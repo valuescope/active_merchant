c35bfe0380e77ca8afdea9cddcb37d52c35ce7a5
Leon Du <leondu@gmail.com>
2011-08-15 17:00:31 +0800

    customize active_merchant
    1) support encrypt data
    2) pass multiple line items to paypal

--- lib/active_merchant/billing/integrations/action_view_helper.rb
+++ lib/active_merchant/billing/integrations/action_view_helper.rb
@@ -62,6 +62,78 @@ module ActiveMerchant #:nodoc:
           concat(result.respond_to?(:html_safe) ? result.html_safe : result)
           nil
         end
+
+        # Paypal form helper, support encrypt data
+        # If you want to encrypt data, pass the following params in options
+        #   :encrypt => true,
+        #   :certs_params => {
+        #     :cert_id => cert_id,
+        #     :cert_dir => 'cert_dir_path',
+        #     :pubcert => 'pubic_cert_path',
+        #     :prvkey => 'private_cert_path',
+        #     :paypal_cert => 'paypal_cert_path'
+        #   }
+        def paypal_payment_service_for(order, account, options = {}, &proc)
+          raise ArgumentError, "Missing block" unless block_given?
+
+          integration_module = ActiveMerchant::Billing::Integrations.const_get(options.delete(:service).to_s.camelize)
+
+          encrypt = options.delete(:encrypt)
+          certs_params = options.delete(:certs_params)
+
+          result = []
+          result << form_tag(integration_module.service_url, options.delete(:html) || {})
+
+          service_class = integration_module.const_get('Helper')
+          service = service_class.new(order, account, options)
+
+          result << capture(service, &proc)
+
+          if encrypt
+            paypal_params = { :cert_id => certs_params[:cert_id] }
+
+            service.form_fields.each do |field, value|
+              paypal_params.merge!(field => value)
+            end
+
+            result << hidden_field_tag(:cmd, "_s_xclick")
+            result << "\n"
+            result << hidden_field_tag(:encrypted, encrypt_for_paypal(paypal_params, certs_params))
+          else
+            service.form_fields.each do |field, value|
+              result << hidden_field_tag(field, value)
+            end
+          end
+
+          result << '</form>'
+          result= result.join("\n")
+
+          concat(result.respond_to?(:html_safe) ? result.html_safe : result)
+          nil
+        end
+
+        # encrypt values
+        # if you put you certs_params in:
+        #   #{Rails.root}/config/paypal
+        # and name them as:
+        #   paypal-pubcert.pem
+        #   paypal-prvkey.pem
+        #   paypal-cert.pem
+        # then you can use this without passing any params
+        # or you should pass the path params to tell where to find the certs
+        def encrypt_for_paypal(values, options = {})
+          cert_dir = options[:cert_dir] || "#{Rails.root}/config/paypal"
+          pubcert_file = options[:pubcert_file] || "paypal-pubcert.pem"
+          prvcert_file = options[:pubcert_file] || "paypal-prvkey.pem"
+          paypal_cert_file = options[:pubcert_file] || "paypal-cert.pem"
+
+          app_cert_pem = File.read("#{cert_dir}/#{options[:pubcert_file]}")
+          app_key_pem = File.read("#{cert_dir}/#{options[:prvcert_file]}")
+          paypal_cert_pem = File.read("#{cert_dir}/#{options[:paypal_cert_file]}")
+
+          signed = OpenSSL::PKCS7::sign(OpenSSL::X509::Certificate.new(app_cert_pem), OpenSSL::PKey::RSA.new(app_key_pem, ''), values.map { |k, v| "#{k}=#{v}" }.join("\n"), [], OpenSSL::PKCS7::BINARY)
+          OpenSSL::PKCS7::encrypt([OpenSSL::X509::Certificate.new(paypal_cert_pem)], signed.to_der, OpenSSL::Cipher::Cipher::new("DES3"), OpenSSL::PKCS7::BINARY).to_s.gsub("\n", "")
+        end
       end
     end
   end
--- lib/active_merchant/billing/integrations/paypal/helper.rb
+++ lib/active_merchant/billing/integrations/paypal/helper.rb
@@ -22,15 +22,29 @@ module ActiveMerchant #:nodoc:
 
           def initialize(order, account, options = {})
             super
-            add_field('cmd', '_ext-enter')
-            add_field('redirect_cmd', '_xclick')
-            add_field('quantity', 1)
-            add_field('item_name', 'Store purchase')
+            # indecate we are using thirdparty shopping cart
+            add_field('cmd', '_cart')
+            add_field('upload', '1')
             add_field('no_shipping', '1')
             add_field('no_note', '1')
             add_field('charset', 'utf-8')
             add_field('address_override', '0')
             add_field('bn', application_id.to_s.slice(0,32)) unless application_id.blank?
+          end
+
+          # pass the shipping cost for whole cart
+          mapping :shipping, 'handling_cart'
+
+          # mapping header image
+          mapping :cpp_header_image, 'cpp_header_image'
+
+          # add line item details, note the amount_x should be price instead of line total
+          def line_items(items = [])
+            items.each_with_index do |line, index|
+              add_field("item_name_#{index+1}", line.item.item_name)
+              add_field("amount_#{index+1}", line.price.value_without_unit)
+              add_field("quantity_#{index+1}", line.quantity)
+            end
           end
 
           mapping :amount, 'amount'
